00648967518315e55b255542dd682c002fcfefb0
wissalbouyahya <wissalbouyahya508@gmail.com>
2026-03-29 15:45:16 +0200

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,119 @@
+package com.projet.DocCarePortal.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPatient;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+    private String adresseP;
+    private Date dateNaissance;
+    private String numeroTel;
+    private String qrcode;
+    private String status;
+    private Boolean etat;
+
+    public Patient() {}
+
+    public Long getIdPatient() {
+        return idPatient;
+    }
+
+    public void setIdPatient(Long idPatient) {
+        this.idPatient = idPatient;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getAdresseP() {
+        return adresseP;
+    }
+
+    public void setAdresseP(String adresseP) {
+        this.adresseP = adresseP;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getNumeroTel() {
+        return numeroTel;
+    }
+
+    public void setNumeroTel(String numeroTel) {
+        this.numeroTel = numeroTel;
+    }
+
+    public String getQrcode() {
+        return qrcode;
+    }
+
+    public void setQrcode(String qrcode) {
+        this.qrcode = qrcode;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+    public Boolean getEtat() {
+        return etat;
+    }
+
+    
+    public void setEtat(boolean etat) {
+        this.etat = etat;
+    }
+}

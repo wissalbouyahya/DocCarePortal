77372ac99f1fdc9877f02d73902e3abf912a194d
sarraneji <sarraneji2910@gmail.com>
2026-04-17 10:20:26 +0200

    Create Admin.Entity.ts

---
+++ Admin.Entity.ts
@@ -0,0 +1,11 @@
+export class Admin {
+    constructor(
+        public id?: number,
+        public nom?: string,
+        public prenom?: string,
+        public email?: string,
+        public mdp?: string,
+        
+        public role?: string
+    ) {}
+}

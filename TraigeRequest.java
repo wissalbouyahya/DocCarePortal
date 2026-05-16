5bde1e318466f1e3c867f148a7eefa4910e181a4
sarraneji <sarraneji2910@gmail.com>
2026-05-16 23:36:31 +0200

    Create TraigeRequest.java

---
+++ TraigeRequest.java
@@ -0,0 +1,9 @@
+package com.projet.DocCarePortal.Entity;
+
+public class TriageRequest {
+
+    private String symptomes;
+
+    public String getSymptomes() { return symptomes; }
+    public void setSymptomes(String symptomes) { this.symptomes = symptomes; }
+}

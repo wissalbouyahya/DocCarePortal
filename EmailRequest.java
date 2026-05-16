7cd861e051ea1b9f67d022f3f9ca4cafff793b14
sarraneji <sarraneji2910@gmail.com>
2026-05-16 23:33:41 +0200

    Create EmailRequest.java

---
+++ EmailRequest.java
@@ -0,0 +1,12 @@
+package com.projet.DocCarePortal.Entity;
+
+public class EmailRequest{
+    private String reference;
+    private String email;
+
+    public String getReference() { return reference; }
+    public void setReference(String reference) { this.reference = reference; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
